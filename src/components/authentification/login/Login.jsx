e091da3f894c1ca636adc334e7b7629d512c93a6
be.0x686172 <abderahmane.beharrahala@gmail.com>
2025-09-30 01:53:44 +0200

    => Continuer le front de l'authentification

--- src/components/authentification/login/Login.jsx
+++ src/components/authentification/login/Login.jsx
@@ -4,10 +4,14 @@ const Login = ({ setMode }) => {
     return (
         <div className='component c-Login'>
             <form>
-                <label>Email</label>
-                <input type="email" placeholder='Email address' />
-                <label>Password</label>
-                <input type="password" placeholder='Password' />
+                <div>
+                    <label>Email</label>
+                    <input type="email" placeholder='Email address' />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="password" placeholder='Password' />
+                </div>
                 <input type="submit" />
             </form>
             <button onClick={() => {setMode('register')}}>Register</button>
--- src/components/authentification/register/Register.jsx
+++ src/components/authentification/register/Register.jsx
@@ -3,6 +3,21 @@ import './style.scss';
 const Register = ({ setMode }) => {
     return (
         <div className='component c-Register'>
+            <form>
+                <div>
+                    <label>Email</label>
+                    <input type="email" placeholder='Email address' />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="password" placeholder='Password' />
+                </div>
+                <div>
+                    <label>Confirm Password</label>
+                    <input type="password" placeholder='Confirm Password' />
+                </div>
+                <input type="submit" />
+            </form>
             <button onClick={() => {setMode('login')}}>Login</button>
         </div>
     );
