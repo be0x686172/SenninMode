6991bdbb892b40374a34fc4a29b48f0f5140a237
be0x686172 <abderahmane.beharrahala@gmail.com>
2025-10-12 12:08:46 +0200

    page(auth): Implemented the authentication mode toggle animation | Set up the authentication's back-end.

--- src/components/features/authentication/authenticationToggle/AuthenticationToggle.jsx
+++ src/components/features/authentication/authenticationToggle/AuthenticationToggle.jsx
@@ -1,10 +1,29 @@
 import './style.scss';
 
 const AuthenticationToggle = ({ authMode, setAuthMode }) => {
+
+    function changeMode(event)
+    {
+        const slider = document.querySelector('.d-slider');
+        const targetButton = event.target.innerHTML == "Sign In" ? "signIn" : "signUp";
+
+        if (authMode != targetButton)
+        {
+            setAuthMode(targetButton);
+
+            const slideClass = targetButton == "signUp" 
+                ? ["activeToggleSlideToRight", "activeToggleSlideToLeft"]
+                : ["activeToggleSlideToLeft", "activeToggleSlideToRight"];
+        
+            slider.classList.add(slideClass[0]);
+            slider.classList.remove(slideClass[1]);
+        }
+    }
+
     return (
         <div className='component c-authenticationToggle'>
-            <button className={authMode == 'signIn' ? 'active' : ''} onClick={()=> setAuthMode("signIn")}>Sign In</button>
-            <button className={authMode == 'signUp' ? 'active' : ''} onClick={() => setAuthMode("signUp")}>Sign Up</button>
+            <button className={authMode == 'signIn' ? 'active' : ''} onClick={(event) => changeMode(event)}>Sign In</button>
+            <button className={authMode == 'signUp' ? 'active' : ''} onClick={(event) => changeMode(event)}>Sign Up</button>
             <div className='d-slider'></div>
         </div>
     );
